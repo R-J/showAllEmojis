8cae4d5a671b01fccf426a2cecaacedac0c562d9
R-J <robin.jurinka@gmx.net>
2017-11-09 21:49:57 +0100

    Fix bug where emojis are not surrounded by colons
    Thanks to @egorsmkv for support!

--- class.showallemojis.plugin.php
+++ class.showallemojis.plugin.php
@@ -2,13 +2,13 @@
 $PluginInfo['showAllEmojis'] = [
     'Name' => 'Show All Emojis',
     'Description' => 'Add button to reveal all emojis in dropdown',
-    'Version' => '0.2.0',
+    'Version' => '0.3.0',
     'RequiredApplications' => ['Vanilla' => '>= 2.3'],
     'RequiredPlugins' => ['editor' => '>= 1.8.1'],
     'MobileFriendly' => true,
     'HasLocale' => true,
     'Author' => 'Robin Jurinka',
-    'AuthorUrl' => 'http://vanillaforums.org/profile/r_j',
+    'AuthorUrl' => 'https://open.vanillaforums.com/profile/r_j',
     'License' => 'MIT'
 ];
 
@@ -17,35 +17,18 @@ $PluginInfo['showAllEmojis'] = [
  *
  * Shows a toggle that will expand the emoji drop down to show all available
  * emojis.
- *
- * Open issues:
- * - Doesn't work with "edit comments" since that editor doesn't exist on page
- *   load. This solution must bind to "something" in order to always work...
  */
 class ShowAllEmojisPlugin extends Gdn_Plugin {
     public function base_render_before($sender) {
         // Attach to all pages.
         $sender->addJsFile('showallemojis.js', 'plugins/showAllEmojis');
-        // Get all emojis which are not already displayed.
-        $emoji = Emoji::instance();
-        $hiddenEmojis = array_diff_key(
-            $emoji->getEmoji(),
-            array_flip($emoji->getEditorList())
-        );
-        // Get their image tag markup and add it to an array.
-        $emojiInfo = [];
-        foreach ($hiddenEmojis as $emojiAlias => $emojiCanonical) {
-            $emojiInfo[] = [
-                'alias' => $emojiAlias,
-                'html' => $emoji->img(
-                    $emoji->getEmojiPath($emojiAlias),
-                    $emojiAlias
-                )
-            ];
-        }
 
+        // Get all emojis currently shown in the editor.
+        $sender->addDefinition(
+            'EditorEmojis',
+            array_values(Emoji::instance()->getEditorList())
+        );
         // Add everything to definitios so that it can be retrieved via js.
-        $sender->addDefinition('HiddenEmojis', $emojiInfo);
         $sender->addDefinition('ShowAll', t('Show All'));
         $sender->addDefinition('ShowLess', t('Show Less'));
     }
--- js/showallemojis.js
+++ js/showallemojis.js
@@ -1,71 +1,81 @@
 jQuery(document).ready(function($) {
-    transformEmojiDropdown();
+    // Create button.
+    var btn = document.createElement( 'button' );
+    btn.classList.add( 'Button' );
+    // Append button to drop down.
+    btn.appendChild( document.createTextNode( gdn.definition( 'ShowAll' ) ) );
 
-    function transformEmojiDropdown() {
-        // Create button.
-        var btn = document.createElement( 'button' );
-        btn.classList.add( 'Button' );
-        // Append button to drop down.
-        btn.appendChild( document.createTextNode( gdn.definition( 'ShowAll' ) ) );
+    // Create container for button and hidden emojis
+    var container = document.createElement( 'div' );
+    container.classList.add( 'ShowAllEmojis' );
+    container.appendChild( btn );
+    emojiContainer = document.createElement( 'span' );
+    emojiContainer.classList.add( 'HiddenEmojiContainer', 'Hidden' );
 
-        // Create container for button and hidden emojis
-        var container = document.createElement( 'div' );
-        container.classList.add( 'ShowAllEmojis' );
-        container.appendChild( btn );
-        emojiContainer = document.createElement( 'span' );
-        emojiContainer.classList.add( 'HiddenEmojiContainer', 'Hidden' );
+    // Loop throughs remaining emojis and add them to a hidden container.
+    var hiddenEmojis = gdn.definition( 'HiddenEmojis' );
+    var blueprint = document.createElement( 'span' );
+    blueprint.classList.add(
+        'editor-action',
+        'editor-dialog-fire-close',
+        'emoji-wrap'
+    );
+    blueprint.setAttribute( 'data-wysihtml5-command', 'insertHTML' );
 
-        // Loop throughs remaining emojis and add them to a hidden container.
-        var hiddenEmojis = gdn.definition( 'HiddenEmojis' );
-        var blueprint = document.createElement( 'span' );
-        blueprint.classList.add(
-            'editor-action',
-            'editor-dialog-fire-close',
-            'emoji-wrap'
-        );
-        blueprint.setAttribute( 'data-wysihtml5-command', 'insertHTML' );
-        hiddenEmojis.forEach( function( emojiInfo ) {
+    var emojiInfo = gdn.definition('emoji');
+    var editorEmojis = gdn.definition('EditorEmojis');
+
+    var format = emojiInfo.format.replace('{src}', emojiInfo.assetPath + '/{src}');
+    
+    // Loop through all emojis
+    Object.keys(emojiInfo.emoji).forEach(function(key,index) {
+        // Ensure emoji is not already displayed.
+        if (editorEmojis.indexOf(key) == -1) {
             var emoji = blueprint.cloneNode( false );
-            emoji.classList.add( 'emoji-' + emojiInfo['alias'] );
+            emoji.classList.add( 'emoji-' + key );
             emoji.setAttribute(
                 'data-wysihtml5-command-value',
-                ' :' + emojiInfo['alias'] + ': '
+                ' :' + key + ': '
             );
-            emoji.setAttribute( 'title', emojiInfo['alias'] );
+            emoji.setAttribute( 'title', ':' + key + ':' );
             emoji.setAttribute(
                 'data-editor',
-                '{"action":"emoji","value":' + emojiInfo['alias'] + '}'
+                '{"action":"emoji","value":":' + key + ':"}'
             );
-            emoji.innerHTML = emojiInfo['html'];
+            // Append emojis file name to assetPath.
+            var output = format.replace(/{src}/g, emojiInfo.emoji[key]);
+            // Insert emojis :name:
+            output = output.replace(/{name}/g, ':' + key + ':');
+            emoji.innerHTML = output;
             emojiContainer.appendChild( emoji );
+        }
+    });
+    
+    // On click change text and show all emojis
+    btn.addEventListener('click', function (evt) {
+        // Toggle button text.
+        var txt = this.textContent;
+        if ( txt == gdn.definition( 'ShowAll' )) {
+            this.textContent = gdn.definition( 'ShowLess' );
+        } else {
+            this.textContent = gdn.definition( 'ShowAll' );
+        }
+        // Toggle "more" emojis visibility.
+        this.parentNode.nextSibling.classList.toggle( 'Hidden' );
+
+        // Don't follow buttons action.
+        evt.preventDefault();
+        return false;
+    });
+
+    // Attach elements to drop down.
+    $( '.editor-action-emoji > .editor-insert-dialog' ).append( container ).append( emojiContainer );
+
+    // Attach elements to "edit" boxes. Thanks to James Ducker (@jamesinc)
+    // for pointing me in the right direction!
+    $( document ).on( "EditCommentFormLoaded", function ( evt, ctx ) {
+        ctx.find( ".EditCommentForm" ).find( '.editor-action-emoji > .editor-insert-dialog' ).each( function () {
+             $( this ).append( container ).append( emojiContainer );
         });
-
-        // On click change text and show all emojis
-        btn.addEventListener('click', function (evt) {
-            // Toggle button text.
-            var txt = this.textContent;
-            if ( txt == gdn.definition( 'ShowAll' )) {
-                this.textContent = gdn.definition( 'ShowLess' );
-            } else {
-                this.textContent = gdn.definition( 'ShowAll' );
-            }
-            // Toggle "more" emojis visibility.
-            this.parentNode.nextSibling.classList.toggle( 'Hidden' );
-
-            // Don't follow buttons action.
-            evt.preventDefault();
-            return false;
-        });
-
-        // Attach elements to drop down.
-        $( '.editor-action-emoji > .editor-insert-dialog' ).append( container ).append( emojiContainer );
-
-        // Attach elements to "edit" boxes. Thanks to James Ducker (@jamesinc)
-        // for pointing me in the right direction!
-        $( document ).on( "EditCommentFormLoaded", function ( evt, ctx ) {
-            ctx.find( ".EditCommentForm" ).find( '.editor-action-emoji > .editor-insert-dialog' ).each( function () {
-                 $( this ).append( container ).append( emojiContainer );
-            });
-        });
-    }
+    });
 });
